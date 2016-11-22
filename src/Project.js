d3e3a7420165bf149318a61841492c8546efc122
Joyce <jhuang3@uw.edu>
2016-11-22 04:33:22 -0800

    img not showing up

--- src/Project.js
+++ src/Project.js
@@ -6,7 +6,7 @@ var Project = React.createClass({
 	
     render() {
     	var base = 'http://students.washington.edu/jhuang3/info343/';
-    	var imgBase= '../img/';
+    	var imgBase= 'img/';
         return (
         	<div className="flex-item box">
         	<a target="_blank" href={base+this.props.url}><Card  
